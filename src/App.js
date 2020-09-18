2db572dbac56bbbcc391f8b0212067024c448e8a
albeec <albeec0902@gmail.com>
2020-09-17 23:45:48 -0700

    fix typo error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
-import Header from './component/header/header.componenet';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './component/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
@@ -17,14 +17,14 @@ class App extends React.Component {
   componentDidMount() {
     const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()      
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
       }
@@ -33,26 +33,29 @@ class App extends React.Component {
     });
   }
 
-  componenetWillUnmount() {
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
-  render () {
+  render() {
     return (
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' component= {HomePage} />
-          <Route path='/shop' component= {ShopPage} />
-          <Route path='/signin' component= {SignInAndSignUpPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
-  }  
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  null,
+  mapDispatchToProps
+)(App); 
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
 
-    switch(state.type){
+    switch(action.type){
         case 'SET_CURRENT_USER': 
             return {
                 ...state,
@@ -16,3 +16,4 @@ const userReducer = (state = INITIAL_STATE, action) => {
 };
 
 export default userReducer;
+
