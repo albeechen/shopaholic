efe55021e4d778487a7fa06b72339062e788533b
albeec <albeec0902@gmail.com>
2020-09-21 00:33:34 -0700

    added addItem and removeItem functionality to checkout item component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-import { toggleCartHidden } from './cart/cart.action';
+
 
 export default combineReducers({
     user: userReducer,
