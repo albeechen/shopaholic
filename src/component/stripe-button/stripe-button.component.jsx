a4472072837f08b5379f116ecea8f1ddde31afe1
albeec <albeec0902@gmail.com>
2020-09-23 00:15:49 -0700

    updating stripe integration and updating our favicon and title

---
+++ src/component/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HURLUHH4jf4vhBzOwR8NfLurtzZP3jqF73V14mjog8nGuYFgpAauYW76YaPaS0mwozPo2IEZ6jOY6kRybFm3D7s00Xbo6Gnqi';   
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='SHOPAHOLIC Lted.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+
+        />
+    );
+};
+
+export default StripeCheckoutButton;
