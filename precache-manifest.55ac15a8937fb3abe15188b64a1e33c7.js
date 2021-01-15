67dd3e53b3b6cca9f67f99f2b0eb24c3555e8aec
albeec <albeec0902@gmail.com>
2021-01-15 00:10:12 -0800

    Updates

---
+++ precache-manifest.55ac15a8937fb3abe15188b64a1e33c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb87c7019a642d2b9d97d156d2a9d22",
+    "url": "/shopaholic/index.html"
+  },
+  {
+    "revision": "6a59dc058150ea7a3ed4",
+    "url": "/shopaholic/static/css/main.f09d6b04.chunk.css"
+  },
+  {
+    "revision": "65a1f0f67c68d78b4900",
+    "url": "/shopaholic/static/js/2.4cc3bf8a.chunk.js"
+  },
+  {
+    "revision": "4b6ff87288029a7b288110f445284443",
+    "url": "/shopaholic/static/js/2.4cc3bf8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a59dc058150ea7a3ed4",
+    "url": "/shopaholic/static/js/main.44921e65.chunk.js"
+  },
+  {
+    "revision": "46479f309c28344f8eb1",
+    "url": "/shopaholic/static/js/runtime-main.7c2018bb.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shopaholic/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/shopaholic/static/media/shopping-bag.30f07071.svg"
+  }
+]);
